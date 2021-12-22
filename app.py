1fe71d20b7fd318d40a1a0ce2629622853469831
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-22 01:23:02 +0000

    update to python3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def app(env, start_response):
 
     http_status = "200 OK"
     response_headers = [("Content-Type", "text/html")]
-    response_text = "Hello World"
+    response_text = b"Hello World"
 
     start_response(http_status, response_headers)
     return [response_text]
